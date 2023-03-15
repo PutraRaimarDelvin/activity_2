2cead030fe971cfc7f9897a9859b174743404378
Ko Delvin <delvinputra805@gmail.com>
2023-03-15 16:22:05 +0700

    first commit

---
+++ latihan1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main (){ //begin
+//numeriuc luas, P, L;
+//Dsiplay, "Masukkan Panjangnya ; "
+cout << "Masukkan Panjangnya : ";
+//Accept P
+cin >> p;
+//Display, "Masukkan Lebarnya : 
+cout << " Masukkan Lebarnya : ";
+//Accept L
+cin >> L;
+//Compute Luas = P*L
+Luas = P*L
+//Display, " Luasnya : " + Luas
+cout << "Luasnya : " << Luas;
+//end
+
+}
